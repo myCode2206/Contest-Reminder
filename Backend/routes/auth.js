2a2071dd9901c039a2325806f6c72c03234bab12
myCode2206 <rajatyt2206@gmail.com>
2024-08-10 20:06:12 +0530

    new route added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,9 +4,10 @@ const User = require("../models/User");
 const passport = require("passport");
 const sendMail = require("../helper/sendMail");
 
-router.get("/auth", (req, res) => {
+router.get("/auth/hello", (req, res) => {
   res.status(200).json({ msg: "Hello from auth routes" });
 });
+
 router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   const user = new User({ username, email });
