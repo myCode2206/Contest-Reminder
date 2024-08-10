e653595a37a29bcee5a42d6aa33b439ed80040dc
myCode2206 <rajatyt2206@gmail.com>
2024-08-10 18:52:36 +0530

    callback url added

--- Backend/index.js
+++ Backend/index.js
@@ -70,7 +70,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/google/callback",
+      callbackURL:
+        "https://contest-reminder-backend.vercel.app/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
